93f94eb5a6dffaf5291e32a34d6381fe8816319b
Brian Phan <b.phan@berkeley.edu>
2021-12-31 20:47:59 -0800

    Add proper schema associations

--- lib/wanna_backend/accounts/user.ex
+++ lib/wanna_backend/accounts/user.ex
@@ -1,16 +1,25 @@
 defmodule WannaBackend.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias WannaBackend.Accounts.User
   alias WannaBackend.Event
+  alias WannaBackend.Group
+  alias WannaBackend.Socials.Friendship
 
   schema "users" do
     field :first_name, :string
     field :last_name, :string
     field :username, :string
-    many_to_many :events, Event, join_through: "user_events"
-    many_to_many :friends, User,
+    has_many :friendships_from, Friendship, foreign_key: :from_user_id
+    has_many :friendships_to, Friendship, foreign_key: :to_user_id
+    many_to_many :friends_from, User,
       join_through: "friendships",
       join_keys: [from_user_id: :id, to_user_id: :id]
+    many_to_many :friends_to, User,
+      join_through: "friendships",
+      join_keys: [to_user_id: :id, from_user_id: :id]
+    many_to_many :events, Event, join_through: "user_events"
+    many_to_many :groups, Group, join_through: "user_groups"
 
     timestamps()
   end
--- lib/wanna_backend/socials/friendship.ex
+++ lib/wanna_backend/socials/friendship.ex
@@ -1,11 +1,14 @@
 defmodule WannaBackend.Socials.Friendship do
   use Ecto.Schema
   import Ecto.Changeset
+  alias WannaBackend.Accounts.User
 
   schema "friendships" do
     field :accepted, :boolean, default: false
     field :from_user_id, :id
     field :to_user_id, :id
+    belongs_to :from_user, User, define_field: false
+    belongs_to :to_user, User, define_field: false
 
     timestamps()
   end
@@ -14,6 +17,6 @@ defmodule WannaBackend.Socials.Friendship do
   def changeset(friendship, attrs) do
     friendship
     |> cast(attrs, [:accepted])
-    |> validate_required([:accepted])
+    |> validate_required([])
   end
 end
